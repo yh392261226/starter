10bd3f7b739d7ac58b588557eae38b93d57f9fa4
空格不在 <yh392261226@sina.com>
2024-08-04 20:14:01 +0800

    update git modules

---
+++ lua/plugins/bak/vscode.lua
@@ -0,0 +1,22 @@
+if true then
+  return {}
+end
+
+vim.g.vscode = 1
+
+return {
+  "dial.nvim",
+  "flit.nvim",
+  "lazy.nvim",
+  "leap.nvim",
+  "mini.ai",
+  "mini.comment",
+  "mini.move",
+  "mini.pairs",
+  "mini.surround",
+  "nvim-treesitter",
+  "nvim-treesitter-textobjects",
+  "nvim-ts-context-commentstring",
+  "vim-repeat",
+  "yanky.nvim",
+}
